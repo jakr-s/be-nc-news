d54f8fd4bbdd23eda53f479a38547cbdc67bd14e
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2024-10-14 14:06:28 +0100

    Refactor error handling middleware to seperate from app.js

---
+++ app/middleware/errorHandler.js
@@ -0,0 +1,11 @@
+const errorHandler = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    res.status(500).send({ msg: "Internal Server Error" });
+  }
+};
+
+module.exports = errorHandler;
