d4b6fd6331b4578d7b1242cf94ffd9c43abd5abd
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2024-10-14 10:54:51 +0100

    add /api endpoint

--- app/app.js
+++ app/app.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { getTopics } = require('./controllers/controllers');
+const { getTopics } = require("./controllers/controllers");
+const endpoints = require("../endpoints.json");
 
-app.get('/api/topics', getTopics);
+app.get("/api", (req, res) => {
+  res.status(200).json(endpoints);
+});
+
+app.get("/api/topics", getTopics);
 
 // Error handling middleware
 app.use((req, res, next) => {
-  res.status(404).send({ msg: 'Route not found' });
+  res.status(404).send({ msg: "Route not found" });
 });
 
 module.exports = app;
