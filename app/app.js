a53bb2265f54997afbb2f2f61ac2599a309a90b1
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2024-10-14 14:04:49 +0100

    Refactor error handling middleware and handle 404 for undefined routes

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { getTopics, getArticleById } = require("./controllers/controllers");
 const endpoints = require("../endpoints.json");
+const errorHandler = require("./middleware/errorHandler");
 
 app.get("/api", (req, res) => {
   res.status(200).send(endpoints);
@@ -10,9 +11,13 @@ app.get("/api", (req, res) => {
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
 
-// Error handling middleware
+// Handle 404 for undefined routes
 app.use((req, res, next) => {
-  res.status(404).send({ msg: "Route not found" });
+  res.status(404).send({ msg: "Resource not found" });
 });
+
+// Error handling middleware
+app.use(errorHandler);
+
 
 module.exports = app;
