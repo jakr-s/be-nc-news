6c773b16cf57cf6ce5ada48092306fa572f59fbb
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2024-10-14 15:56:11 +0100

    Add GET /api/articles/:article_id/comments endpoint

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,5 +1,5 @@
 const { fetchTopics } = require("../models/topics");
-const { fetchArticleById, fetchAllArticles } = require("../models/articles");
+const { fetchArticleById, fetchAllArticles, fetchCommentsByArticleId } = require("../models/articles");
 
 exports.getTopics = async (req, res, next) => {
   try {
@@ -27,6 +27,19 @@ exports.getAllArticles = async (req, res, next) => {
   try {
     const articles = await fetchAllArticles();
     res.status(200).send({ articles });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getCommentsByArticleId = async (req, res, next) => {
+  try {
+    const { article_id } = req.params;
+    const comments = await fetchCommentsByArticleId(article_id);
+    if (!comments.length) {
+      return next({ status: 404, msg: "Comments not found" });
+    }
+    res.status(200).send({ comments });
   } catch (err) {
     next(err);
   }
--- app/models/articles.js
+++ app/models/articles.js
@@ -27,3 +27,14 @@ exports.fetchAllArticles = async () => {
   `);
   return result.rows;
 };
+
+exports.fetchCommentsByArticleId = async (article_id) => {
+  const result = await db.query(
+    `SELECT comment_id, votes, created_at, author, body, article_id
+     FROM comments
+     WHERE article_id = $1
+     ORDER BY created_at DESC;`,
+    [article_id]
+  );
+  return result.rows;
+};
