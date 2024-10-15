aa3fcedd5c77275a41c142cf7ff167e6a061d050
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2024-10-15 12:02:58 +0100

    Whitespace formatting

--- app/models/topics.js
+++ app/models/topics.js
@@ -1,6 +1,6 @@
-const db = require('../../db/connection');
+const db = require("../../db/connection");
 
 exports.fetchTopics = async () => {
-  const result = await db.query('SELECT slug, description FROM topics;');
+  const result = await db.query("SELECT slug, description FROM topics;");
   return result.rows;
 };
