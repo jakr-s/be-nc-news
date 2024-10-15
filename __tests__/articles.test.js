eca4259bc6a176beece61982c4d3655ecf777183
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2024-10-15 18:40:42 +0100

    Add assertion for comment count property to get article id test

--- __tests__/articles.test.js
+++ __tests__/articles.test.js
@@ -22,8 +22,7 @@ describe("/api/articles/:article_id", () => {
           topic: "mitch",
           created_at: expect.any(String),
           votes: 100,
-          article_img_url:
-            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          comment_count: expect.any(String),
         })
       );
     });
