6691edc6bbde24a9933ae742b33585729b9c1a4b
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2024-10-17 09:17:09 +0100

    add tests for GET /api/users/:username endpoint

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -10,7 +10,9 @@ afterAll(() => db.end());
 
 describe("GET /api/users", () => {
   test("200: responds with an array of users", async () => {
-    const { body: { users } } = await request(app).get("/api/users").expect(200);
+    const {
+      body: { users },
+    } = await request(app).get("/api/users").expect(200);
     expect(users).toBeInstanceOf(Array);
     users.forEach((user) => {
       expect(user).toEqual(
@@ -20,6 +22,29 @@ describe("GET /api/users", () => {
           avatar_url: expect.any(String),
         })
       );
+    });
+  });
+
+  describe("GET /api/users/:username", () => {
+    test("200: responds with a user object", async () => {
+      const {
+        body: { user },
+      } = await request(app).get("/api/users/butter_bridge").expect(200);
+      expect(user).toEqual(
+        expect.objectContaining({
+          username: "butter_bridge",
+          name: "jonny",
+          avatar_url:
+            "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+        })
+      );
+    });
+
+    test("404: responds with an error message when user not found", async () => {
+      const { body } = await request(app)
+        .get("/api/users/nonexistent_user")
+        .expect(404);
+      expect(body.msg).toBe("User not found");
     });
   });
 });
